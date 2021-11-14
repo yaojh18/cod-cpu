da6953bd8946be722795a57873ba28af03714fb8
yaojh18 <yaojh18@mails.tsinghua.edu.cn>
2021-11-14 11:24:42 +0800

    小改了一下

--- testcases/data_conflict/code.asm
+++ testcases/data_conflict/code.asm
@@ -5,11 +5,6 @@ code.elf:     file format elf32-littleriscv
 Disassembly of section .text:
 
 00000000 <_start>:
-   0:	00100293          	li	t0,1
-   4:	00200313          	li	t1,2
-   8:	006283b3          	add	t2,t0,t1
-
-0000000c <end>:
-   c:	00000063          	beqz	zero,c <end>
-  10:	00000013          	nop
-  14:	00000013          	nop
+   0:	00000063          	beqz	zero,0 <_start>
+   4:	00000013          	nop
+   8:	00000013          	nop
